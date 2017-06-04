5e99cbdfecb9eb1fa4bd32a88203c6dbc5d8cb9e
Nikita Korobkov <nikkorobk@gmail.com>
2017-06-03 22:41:50 -0700

    Recursive dir support

--- conf.py
+++ conf.py
@@ -5,3 +5,5 @@ LOADPATH = 'raw_photos'
 SAVEPATH = 'croped_photos'
 
 LF,UP,RG,LO = 660,0,1890,1783
+
+RECURSIVE = True
--- script.py
+++ script.py
@@ -2,25 +2,38 @@ __author__ = 'Insolia'
 
 from os import listdir, mkdir
 from conf import *
-from os.path import isfile, join
+from os.path import isfile, isdir, join
 from PIL import Image
-
 
 loadpath = LOADPATH
 savepath = SAVEPATH
 
-try:
-    mkdir(savepath)
-except(OSError):
-    pass
+def process_dir(local_loadpath):
+    try:
+        mkdir(local_loadpath.replace(loadpath, savepath))
+    except(OSError):
+        pass
 
-raw_photo_names = [f for f in listdir(loadpath) if isfile(join(loadpath, f)) and f[0] != '.']
+    object_names_in_folder = listdir(local_loadpath)
+    print "start processing folder " + local_loadpath + " need to crop " + str(len(listdir(local_loadpath))) + " pics or folders"
+    for f in object_names_in_folder:
+        local_object = join(local_loadpath, f)
+        if isfile(local_object) and f[0] != '.':
+            process_photo(join(local_loadpath, f))
+            continue
 
-for f in raw_photo_names:
-    ph = Image.open(join(loadpath, f))
+        if isdir(join(local_loadpath, f)) and RECURSIVE:
+            process_dir(join(local_loadpath, f))
+            continue
 
+
+def process_photo(photo):
+    ph = Image.open(photo)
     new_p = ph.crop((LF, UP, RG, LO))
+    save_url = photo.replace(loadpath,savepath)
+    new_p.save(save_url)
+    print 'succes at ' + join(savepath, photo.replace(loadpath + "/",""))
 
-    new_p.save(join(savepath, f))
 
-    print 'succes at ' + join(savepath, f)
+if __name__ == '__main__':
+    process_dir(loadpath)
