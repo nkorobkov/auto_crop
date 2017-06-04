4c09499ee9eb010181671556a551075900c7f3f7
Nikita Korobkov <nikkorobk@gmail.com>
2017-06-03 22:58:19 -0700

    Code cleanup

--- script.py
+++ script.py
@@ -5,35 +5,40 @@ from conf import *
 from os.path import isfile, isdir, join
 from PIL import Image
 
-loadpath = LOADPATH
-savepath = SAVEPATH
 
-def process_dir(local_loadpath):
-    try:
-        mkdir(local_loadpath.replace(loadpath, savepath))
-    except(OSError):
-        pass
-
-    object_names_in_folder = listdir(local_loadpath)
-    print "start processing folder " + local_loadpath + " need to crop " + str(len(listdir(local_loadpath))) + " pics or folders"
+def process_dir(loadpath):
+    mirror_dir_for_save(loadpath)
+    object_names_in_folder = listdir(loadpath)
+    print "start processing folder " + loadpath + " need to crop " + str(
+        len(object_names_in_folder)) + " pics or folders"
     for f in object_names_in_folder:
-        local_object = join(local_loadpath, f)
+        local_object = join(loadpath, f)
+
         if isfile(local_object) and f[0] != '.':
-            process_photo(join(local_loadpath, f))
+            process_photo(join(loadpath, f))
             continue
 
-        if isdir(join(local_loadpath, f)) and RECURSIVE:
-            process_dir(join(local_loadpath, f))
+        if isdir(join(loadpath, f)) and RECURSIVE:
+            process_dir(join(loadpath, f))
             continue
 
 
 def process_photo(photo):
     ph = Image.open(photo)
     new_p = ph.crop((LF, UP, RG, LO))
-    save_url = photo.replace(loadpath,savepath)
+    save_url = photo.replace(LOADPATH, SAVEPATH)
     new_p.save(save_url)
-    print 'succes at ' + join(savepath, photo.replace(loadpath + "/",""))
+    print 'success at ' + save_url
 
+
+def mirror_dir_for_save(loadpath):
+    try:
+        mkdir(loadpath.replace(LOADPATH, SAVEPATH))
+    except(OSError):
+        pass
 
 if __name__ == '__main__':
-    process_dir(loadpath)
+    process_dir(LOADPATH)
+
+
+
